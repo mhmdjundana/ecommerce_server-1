f1b8480a78510b0ebad3d2c69ea76e8bbc4a059e
mhmdjundana <mhmd.jundana@gmail.com>
2020-11-14 17:02:15 +0700

    '5:02'

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 
 const express = require('express')
 const router = require('./routes/')
@@ -6,7 +8,7 @@ const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // body parser
 app.use(express.json())
